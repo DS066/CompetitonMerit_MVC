0ee0506fb41d093c7fc374cddacaf1b22efc94e0
Davinder Singh <DS066@atcltd.atc.org.nz>
2021-02-04 14:05:48 +1300

    Add project files.

---
+++ CompetitonMerit_MVC/Models/Meritlist.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompetitonMerit_MVC.Models
+{
+    public class Meritlist
+    {
+        public int Id { get; set; }
+        public int Student_participateId { get; set; }
+        public Student_participate Student_participate { get; set; }
+
+        public int SchoolId { get; set; }
+        public School School { get; set; }
+
+        public int SubjectId { get; set; }
+        public Subject Subject { get; set; }
+
+        [Required]
+        public int Marks { get; set; }
+    }
+}
---
+++ CompetitonMerit_MVC/Models/Student_participate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompetitonMerit_MVC.Models
+{
+    public class Student_participate
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Student_Name { get; set; }
+        [Required]
+
+        public int Student_age { get; set; }
+
+        [Required]
+        public string Mobile_Number { get; set; }
+
+        [Required]
+        public string Student_Address { get; set; }
+
+        [Required]
+        public string Father_Name { get; set; }
+    }
+}
